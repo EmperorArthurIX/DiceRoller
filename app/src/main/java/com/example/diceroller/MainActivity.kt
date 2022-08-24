fa4ce2e43d9b74ad8934b079ca435d03a2ae940a
Ishaan Sunita Pandita <emperorarthurix@gmail.com>
2022-08-24 13:20:45 +0530

    Replacing Text with Dice Images

--- app/src/main/java/com/example/diceroller/MainActivity.kt
+++ app/src/main/java/com/example/diceroller/MainActivity.kt
@@ -2,7 +2,7 @@ package com.example.diceroller
 
 import android.os.Bundle
 import android.widget.Button
-import android.widget.TextView
+import android.widget.ImageView
 import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
@@ -12,13 +12,33 @@ class MainActivity : AppCompatActivity() {
 
         // Getting Interaction Items
         val rollButton: Button = findViewById(R.id.rollDice)
-        val resultTextView: TextView = findViewById(R.id.rollValue)
+//        val resultTextView: TextView = findViewById(R.id.rollValue)
+        val diceImage: ImageView = findViewById(R.id.rollState)
 
         // Interaction Responses
         rollButton.setOnClickListener {
 //            Toast.makeText(this, "Dice Rolled!", Toast.LENGTH_SHORT).show()
-            resultTextView.text = Dice().roll().toString()
+//            resultTextView.text = Dice().roll().toString()
+            rollDice(diceImage)
         }
+    }
+
+    fun rollDice(diceImage: ImageView)
+    {
+        val diceRoll = Dice().roll().toString()
+        val drawableResource = when(diceRoll) {
+            "1" -> R.drawable.dice_1
+            "2" -> R.drawable.dice_2
+            "3" -> R.drawable.dice_3
+            "4" -> R.drawable.dice_4
+            "5" -> R.drawable.dice_5
+            "6" -> R.drawable.dice_6
+
+            // Default
+            else -> {R.drawable.dice_3}
+        }
+        diceImage.setImageResource(drawableResource)
+        diceImage.contentDescription = diceRoll
     }
 }
 
