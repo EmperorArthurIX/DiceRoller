6d3b0226930109ce07b9a1216f8cedfe170ec7ef
Ishaan Sunita Pandita <emperorarthurix@gmail.com>
2022-08-24 13:45:18 +0530

    Added Number Generation Unit Test

--- app/src/test/java/com/example/diceroller/ExampleUnitTest.kt
+++ app/src/test/java/com/example/diceroller/ExampleUnitTest.kt
@@ -1,8 +1,8 @@
 package com.example.diceroller
 
+import org.junit.Assert.assertEquals
+import org.junit.Assert.assertTrue
 import org.junit.Test
-
-import org.junit.Assert.*
 
 /**
  * Example local unit test, which will execute on the development machine (host).
@@ -13,5 +13,12 @@ class ExampleUnitTest {
     @Test
     fun addition_isCorrect() {
         assertEquals(4, 2 + 2)
+    }
+
+    @Test
+    fun generates_number() {
+        val dice = Dice(6)
+        val diceRoll = dice.roll()
+        assertTrue("The generated value was not within 1 to 6 inclusive", diceRoll in 1..6)
     }
 }
